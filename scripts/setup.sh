8581ae74b1520944bbcda7df09316a32e843a253
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-03 22:52:09 +0900

    git の紐づけ

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+USERS=("yoshiyoshiharu" "KoyamaShimpei" "taiwork")
+REPOSITORY_NAME="git@github.com:yoshiyoshiharu/isucon14-qualify.git"
+
 # authorized_keysにgithubに登録しているssh公開鍵を登録する
-users=("yoshiyoshiharu" "KoyamaShimpei" "taiwork")
-for user in ${users[@]}; do
+for user in ${USERS[@]}; do
   echo $(curl -fs "https://github.com/${user}.keys") >> ~/.ssh/authorized_keys
 done
 echo "authorized_keysにGithubのSSH公開鍵を登録しました"
+
+# git initからremoteとの紐づけまで
+git init
+git remote add origin $REPOSITORY_NAME
+git pull -u origin master
