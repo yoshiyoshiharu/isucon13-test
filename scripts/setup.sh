5a9481d4edb91867ea72ca9fe3e379b68b57deb0
Haruki Yoshida <haruki.osaka.u@gmail.com>
2024-11-04 20:49:06 +0900

    fix prepare.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,8 @@ set -e
 
 USERS=("yoshiyoshiharu" "KoyamaShimpei" "taiwork")
 REPOSITORY_NAME="git@github.com:yoshiyoshiharu/isucon14-qualify.git"
+GIT_USER_NAME=yoshiyoshiharu
+GIT_USER_EMAIL=haruki.osaka.u@gmail.com
 
 # authorized_keysにgithubに登録しているssh公開鍵を登録する
 for user in ${USERS[@]}; do
@@ -13,5 +15,7 @@ echo "authorized_keysにGithubのSSH公開鍵を登録しました"
 
 # git initからremoteとの紐づけまで
 git init
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
+git config --global core.editor "vim"
 git remote add origin $REPOSITORY_NAME
-git pull -u origin master
